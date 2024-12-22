9edb7af7faefe40f7e2d37d607dbc009f185202b
Gustavo Matheus Pauvels <myth.gustavo06@gmail.com>
2024-12-22 02:24:14 -0300

    Update API Service

--- app/src/main/java/com/example/carstore/Control/CarStoreAPIService.java
+++ app/src/main/java/com/example/carstore/Control/CarStoreAPIService.java
@@ -6,46 +6,77 @@ import com.example.carstore.Models.Marca;
 import com.example.carstore.Models.Modelo;
 
 import java.util.List;
-
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Body;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface CarStoreAPIService {
-    //Marcas
+    ////////////////////////////////////////////// MARCAS
     @GET("marcas")
     Call<List<Marca>> createGetMarcas();
 
-    //Cidades
+    @POST
+    Call<Void> createPostMarca(@Body Marca marca);
+
+    @PUT("marcas/{id}")
+    Call<Void> createPutMarca(@Path("id") Long idMarca, @Body Marca novaMarca);
+
+    @DELETE("marcas/{id}")
+    Call<Void> createDeleteMarca(@Path("id") Long idMarca);
+
+    ////////////////////////////////////////////// CIDADES
     @GET("cidades")
     Call<List<Cidade>> createGetCidades();
 
-    //Modelos
+    @POST
+    Call<Void> createPostCidade(@Body Cidade cidade);
+
+    @PUT("cidades/{id}")
+    Call<Void> createPutCidade(@Path("id") Long idCidade, @Body Cidade novaCidade);
+
+    @DELETE("cidades/{id}")
+    Call<Void> createDeleteCidade(@Path("id") Long idCidade);
+
+    ////////////////////////////////////////////// MODELOS
     @GET("modelos")
     Call<List<Modelo>> createGetModelos();
 
-    @GET("modelos")
-    Call<List<Modelo>> createGetModelosByMarca(@Query("marca") Long idMarca);
+    @POST("modelos")
+    Call<Void> createPostModelo(@Body Modelo modelo);
 
-    @GET("modelos/{id}")
-    Call<Modelo> createGetModeloById(@Path("id") Long idModelo);
+    @PUT("modelos/{id}")
+    Call<Void> createPutModelo(@Path("id") Long idModelo, @Body Modelo novoModelo);
 
-//    @POST("modelos")
-//    Call<Void> createPostModelo(@Body Modelo modelo);
-//
-//    @PUT("modelos/{id}")
-//    Call<Void> createPutModelo(@Path("id") Long idModelo, @Body Modelo novoModelo);
-//
-//    @DELETE("modelos/{id}")
-//    Call<Void> createDeleteModelo(@Path("id") Long idModelo);
+    @DELETE("modelos/{id}")
+    Call<Void> createDeleteModelo(@Path("id") Long idModelo);
 
-    //Anuncios
+    ////////////////////////////////////////////// ANUNCIOS
     @GET("anuncios")
     Call<List<Anuncio>> createGetAnuncios();
 
+    @GET("anuncios")
+    Call<List<Anuncio>> createGetAnunciosByFilter(
+            @Query("modelo") Long id,
+            @Query("ano_inicial") Integer anoInicial,
+            @Query("ano_final") Integer anoFinal,
+            @Query("min") Double valorMinimo,
+            @Query("max") Double valorMaximo
+    );
+
+    @GET("anuncios")
+    Call<List<Anuncio>> createGetAnunciosByModelo( @Query("modelo") Long id );
+
     @POST("anuncios")
     Call<Void> createPostAnuncio(@Body Anuncio anuncio);
+
+    @PUT("anuncios/{id}")
+    Call<Void> createPutAnuncio(@Path("id") Long idAnuncio, @Body Anuncio novoAnuncio);
+
+    @DELETE("anuncios/{id}")
+    Call<Void> createDeleteAnuncio(@Path("id") Long idAnuncio);
 }
